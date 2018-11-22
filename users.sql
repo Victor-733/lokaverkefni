47977b4612fd6f6fb4e32dedaa114b3e2842d744
Victor-733 <victorwahid@gmail.com>
2018-11-22 15:12:28 +0000

    add tables to users.sql

---
+++ users.sql
@@ -0,0 +1,29 @@
+create database 1611012220_VEFlokaverkefni;
+use 1611012220_VEFlokaverkefni;
+
+-- ADMINS
+create table admins (
+    user_id int primary key auto_increment not null,
+    username varchar(50) not null,
+    pass varchar(50) not null
+);
+
+-- POSTS
+create table posts (
+    id int primary key auto_increment not null,
+    story text not null,
+    author varchar(50) not null,
+    foreign key (id) references admins(user_id)
+);
+
+---- DROP COMMANDS ----
+drop table admins;
+drop table posts;
+-----------------------
+
+-- INSERT - ADMINS
+insert into admins (username, pass)
+values
+    ("Victor_733", "cheater733");
+
+select * from admins;
